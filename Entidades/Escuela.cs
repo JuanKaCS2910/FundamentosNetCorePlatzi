3fe8f43fd775cc50102b05f492eb4304a023c544
Juan Carlos <juan.castro.socla@gmail.com>
2022-05-25 16:27:30 -0500

    Cargar archivos

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,48 @@
+namespace CoreEscuela.Entidades
+{
+    public enum TiposDeEscuela
+    {
+        Primaria,
+        Secundaria, 
+        PreEscolar
+    }
+
+    
+    public class Escuela 
+    {
+        string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value.ToUpper(); }
+        }
+
+        public int AñoDeCreacion { get; set; }
+
+        public string Pais { get; set; }
+
+        public string Ciudad { get; set; }
+
+        public TiposDeEscuela tipoEscuela { get; set; }
+
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año);
+
+        //Constructor con parámetros opcionales
+        public Escuela(string nombre, int año, 
+                       TiposDeEscuela tipo, 
+                       string pais = "", string ciudad = "")
+        {
+            
+            (Nombre, AñoDeCreacion) = (nombre, año);//Asignación de tuplas.
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+        public override string ToString()
+        {
+            //return $"Nombre : \"{Nombre}\", Tipo : {tipoEscuela} \n Pais : {Pais}, Ciudad :{Ciudad}";
+            return $"Nombre : \"{Nombre}\", Tipo : {tipoEscuela} {System.Environment.NewLine} Pais : {Pais}, Ciudad :{Ciudad}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CoreEscuela.Entidades;
+
+var escuela = new Escuela("San Marcos", 2010,TiposDeEscuela.Primaria,
+                            pais: "Perú", ciudad: "Lima1"
+);
+//escuela.tipoEscuela = TiposDeEscuela.Primaria;
+//escuela.Ciudad = "Lima";
+//escuela.Pais = "Perú";
+Console.WriteLine(escuela);
