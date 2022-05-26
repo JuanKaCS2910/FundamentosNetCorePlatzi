a07b2f26fd3ff840428743adf2fee18c98dbb267
Juan Carlos <juan.castro.socla@gmail.com>
2022-05-25 23:22:36 -0500

    Agregar Arreglos

--- Program.cs
+++ Program.cs
@@ -5,20 +5,28 @@ using FundamentosNetCorePlatzi.Entidades;
 var escuela = new Escuela("San Marcos", 2010,TiposDeEscuela.Primaria,
                             pais: "Perú", ciudad: "Lima1"
 );
-var curso1 = new Curso(){
-        nombre ="101"
-    };
-var curso2 = new Curso(){
-        nombre ="201"
-    };
-var curso3 = new Curso(){
-        nombre ="301"
-    };
+
+var arreglosCursos = new Curso[3];
+arreglosCursos[0]= new Curso(){
+                        nombre ="101"
+                    };
+arreglosCursos[1]= new Curso(){
+                        nombre ="201"
+                    };
+arreglosCursos[2]= new Curso(){
+                    nombre ="301"
+                };
 //escuela.tipoEscuela = TiposDeEscuela.Primaria;
 //escuela.Ciudad = "Lima";
 //escuela.Pais = "Perú";
 Console.WriteLine(escuela);
 Console.WriteLine("================");
-Console.WriteLine("Curso 1 : " + curso1.nombre + " | " + curso1.uniqueId);
-Console.WriteLine($"Curso 2 : {curso2.nombre} | {curso2.uniqueId}");
-Console.WriteLine(curso3);
+// Console.WriteLine("Curso 1 : " + arreglosCursos[0].nombre + " | " + arreglosCursos[0].uniqueId);
+// Console.WriteLine($"Curso 2 : {arreglosCursos[1].nombre} | {arreglosCursos[1].uniqueId}");
+// Console.WriteLine(arreglosCursos[2]);
+
+for (int i = 0; i < arreglosCursos.Length; i++)
+{
+    Console.WriteLine("Curso " + i + " | Nombre : " + arreglosCursos[i].nombre + 
+    " | UniqueId : " + arreglosCursos[i].uniqueId);
+}
